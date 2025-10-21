89474ebf8de2cadc1637eb35052625b6104d4cca
miniproject <kcchinmayi@gmail.com>
2025-10-21 18:01:58 +0530

    Fetch and display movies from TMDB  API

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Moviecard = () => {//Moviecard component to display individual movie cards
+const Moviecard = ({poster_path,name}) => {//Moviecard component to display individual movie cards
   return (
-    <div className='h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 duration-300 hover:cursor-pointer flex flex-col justify-between items-end' style={{backgroundImage:`url(https://tse4.mm.bing.net/th/id/OIP.wd3cv4j7KmVj9uD5FV0xlQHaKO?pid=Api&P=0&h=180)`}}> </div>
+    <div className='h-[40vh] w-[200px] bg-center bg-cover rounded-xl hover:scale-110 duration-300 hover:cursor-pointer flex flex-col justify-between items-end' style={{backgroundImage:`url(https://image.tmdb.org/t/p/w500${poster_path})`}}> 
+    
+    <div className='text-white text-xl bg-gray-900/60 w-full text-center rounded-b-xl p-2'>
+      {name}
+    </div>
+    </div>
   )
 }
 
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
 import Moviecard from './Moviecard'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 const Movies = () => {//Movies component to display a list of movie cards
+ 
+const [movies,setMovies]=React.useState([])//holds list of movies fetched from the api
+
+  useEffect(()=>{
+
+     axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_API_KEY}&language=en-US&page=5`)//replace with ypor api key
+  .then(res => setMovies(res.data.results))
+  },[])
+ 
+ 
+ 
   return (
     <div className='p-5'>
        <div className='text-2xl font-bold text-center m-5'>Trending Movies</div>
    
-        <div className='flex flew-row flex-wrap justify-around gap-7'>
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>  
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>  
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>
-            <Moviecard/>        
+        <div className='flex flex-row flex-wrap justify-around gap-8 m-5'>
+          
+          {movies.map((movieObj)=>{// Maps over the movies array and renders a Moviecard for each movie with its poster and title 
+            return <Moviecard  key={movieObj.id} poster_path={movieObj.poster_path} name={movieObj.original_title}/>//3 pros are passed,unique key, poster_path gives image path and name gives movie title
+          }
+        )
+        }
         </div>
    
     </div>
@@ -29,3 +34,4 @@ const Movies = () => {//Movies component to display a list of movie cards
 }
 
 export default Movies
+
